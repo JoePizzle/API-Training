dcfec546c4bc5f7d436bf7d318dd2bf0e0b67c37
JoePizzle <rzfeeser@noreply.com>
2020-02-05 08:51:33 -0500

    CLIArgumentParsing

---
+++ CLIArgumentParsing01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#Exploring Arguement Parsing
+#Import standard libraies
+
+import sys
+
+args = sys.argv
+print("Username: " + args[1])
+print("Password: " + args[2])
+print("IP Address: " + args[3])
+print("Gateway: " + args[4])
+print("Oh by the way, your script is named:", args[0])
